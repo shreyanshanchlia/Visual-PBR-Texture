0373f0ce819fbc8425cb4aa354d633828df4f3ad
ShreyanshAnchlia <shreyanshanchlia@gmail.com>
2020-11-19 21:12:57 +0530

    Initial Commit

---
+++ Assets/ChangeMaterial.cs
@@ -0,0 +1,69 @@
+﻿/*
+* Author - @Shreyansh Anchlia 
+* ©SquareDragon Games
+*/
+
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+	public Material Mat;
+
+	public Texture[] AlbedoMaps;
+	public Texture[] NormalMaps;
+	public Texture[] MetallicMaps;
+	public Texture[] HeightMaps;
+	public Texture[] OcclusionMaps;
+
+	int index = 0;
+
+	private void Start()
+	{
+		NullMaterial();
+	}
+
+	[ContextMenu("Null Material")]
+	public void NullMaterial()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", null);
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_BumpMap", null);
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_MetallicGlossMap", null);
+		this.gameObject.GetComponent<MeshRenderer>().material.SetFloat("_Glossiness", 0f);
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_ParallaxMap", null);
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_OcclusionMap", null);
+	}
+	public void UpdateMesh()
+	{
+
+	}
+	public void ChangeAlbedoImage()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", AlbedoMaps[++index % AlbedoMaps.Length]);
+	}
+	public void ChangeNormalImage()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_BumpMap", NormalMaps[index % NormalMaps.Length]);
+		this.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_NORMALMAP");
+	}
+	public void ChangeMetallicImage()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_MetallicGlossMap", MetallicMaps[index % MetallicMaps.Length]);
+		this.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_METALLICGLOSSMAP");
+	}
+	public void ChangeSmoothnessSlider(float smoothness)
+	{
+		Mat.SetFloat("_Glossiness", smoothness);
+		this.gameObject.GetComponent<MeshRenderer>().sharedMaterial.EnableKeyword("_SPECGLOSSMAP");
+	}
+	public void ChangeHeightMap()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_ParallaxMap", HeightMaps[index % HeightMaps.Length]);
+		this.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_PARALLAXMAP");
+	}
+	public void ChangeOcclusionMap()
+	{
+		this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_OcclusionMap", OcclusionMaps[index % OcclusionMaps.Length]);
+		this.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_OCCLUSIONMAP");
+	}
+}
---
+++ Assets/MouseMove.cs
@@ -0,0 +1,44 @@
+﻿/*
+* Author - @Shreyansh Anchlia 
+* ©SquareDragon Games
+*/
+
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+	public Camera cam;
+	public Transform pivot;
+	public Transform target;
+
+	public float speed = 1f;
+	public float panSpeed = 0.001f;
+
+	public float minYBounds, maxYBounds;
+	int isRotating = 0; //1 = anticlockwise, -1 = clockwise
+
+	Vector2 prevPos;
+	Vector3 cameraPosition;
+
+
+	private void Update()
+	{
+		cameraPosition = cam.transform.position;
+		pivot.Rotate(Vector3.up, isRotating *  speed);
+		if(Input.GetMouseButton(0) && isRotating == 0)
+		{
+			cameraPosition.y = Mathf.Clamp(cameraPosition.y + (prevPos.y - Input.mousePosition.y) * panSpeed, minYBounds, maxYBounds);
+			cam.transform.position = cameraPosition;
+		}
+		cam.transform.LookAt(target);
+		prevPos = Input.mousePosition;
+	}
+	public void AnticlockWiseRotate()
+	{
+		isRotating++;
+	}
+	public void ClockWiseRotate()
+	{
+		isRotating--;
+	}
+}
